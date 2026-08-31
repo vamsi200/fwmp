9ff438bede56526462798e91a4a5c1942fde0185
vamsi200 <vamsivanama891@gmail.com>
2026-08-31 23:49:37 +0530

    add build.sh and few changes

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIR=$(git rev-parse --show-toplevel)
+
+echo "[INFO] Building bpf object file"
+cd "$DIR" && make
+
+echo "[INFO] Building C3 code and Running"
+cd "$DIR" && sudo c3c run
