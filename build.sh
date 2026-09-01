26b08138502bfdfa205e81e9fb7e9dc5777dbeee
vamsi200 <vamsivanama891@gmail.com>
2026-09-01 10:18:25 +0530

    fix build.sh and prepare for appimage release

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 DIR=$(git rev-parse --show-toplevel)
 
 echo "[INFO] Building bpf object file"
-cd "$DIR" && make
+cd "$DIR/bpf" && make
 
 echo "[INFO] Building C3 code and Running"
 cd "$DIR" && sudo c3c run
